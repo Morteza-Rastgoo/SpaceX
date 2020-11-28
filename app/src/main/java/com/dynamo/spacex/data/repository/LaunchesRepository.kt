02f9a793ae9b8fb38918ad268605432ef4f6c177
Morteza <mmortezarastgoo@gmail.com>
2020-11-28 17:29:23 +0330

    Create LaunchesRepository.kt to be used as repository layer for data gathering for the spaceX launches

---
+++ app/src/main/java/com/dynamo/spacex/data/repository/LaunchesRepository.kt
@@ -0,0 +1,13 @@
+package com.dynamo.spacex.data.repository
+
+import javax.inject.Inject
+import javax.inject.Singleton
+
+/**
+ * @author : Morteza Rastgoo
+ * @since : 28/11/2020 AD
+ **/
+@Singleton
+class LaunchesRepository @Inject constructor() {
+
+}
