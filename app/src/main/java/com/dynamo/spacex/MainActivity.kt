813f703a6312f5ed56147e402eba9de8f1b60158
Morteza <mmortezarastgoo@gmail.com>
2020-11-28 15:52:38 +0330

    Create Navigation Graph and cleanup the MainActivity.kt code

--- app/src/main/java/com/dynamo/spacex/MainActivity.kt
+++ app/src/main/java/com/dynamo/spacex/MainActivity.kt
@@ -1,18 +1,7 @@
 package com.dynamo.spacex
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.dynamo.spacex.ui.main.MainFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.main_activity) {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                    .replace(R.id.container, MainFragment.newInstance())
-                    .commitNow()
-        }
-    }
 }
