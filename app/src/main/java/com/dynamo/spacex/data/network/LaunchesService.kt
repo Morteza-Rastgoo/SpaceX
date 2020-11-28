3a3bf7e9811c33dbdb696e651f04e705ee2da19c
Morteza <mmortezarastgoo@gmail.com>
2020-11-28 17:38:56 +0330

    Add Comment for LaunchesService.kt

--- app/src/main/java/com/dynamo/spacex/data/network/LaunchesService.kt
+++ app/src/main/java/com/dynamo/spacex/data/network/LaunchesService.kt
@@ -1,5 +1,10 @@
 package com.dynamo.spacex.data.network
 
+/**
+ * @author : Morteza Rastgoo
+ * @since : 28/11/2020 AD
+ * This Service will indicate the API calls related to launches of the SpaceX
+ **/
 interface LaunchesService {
 
 
