a45a1440b7a1f8a5f46fc41ce454a982b1f11329
Morteza <mmortezarastgoo@gmail.com>
2020-11-28 18:06:15 +0330

    Add Optional Output Control QueryStrings to the past Launches request

--- app/src/main/java/com/dynamo/spacex/data/network/LaunchesService.kt
+++ app/src/main/java/com/dynamo/spacex/data/network/LaunchesService.kt
@@ -12,11 +12,24 @@ import retrofit2.http.Query
  **/
 interface LaunchesService {
 
+    /**
+     * Call to show the Past Launches
+     *  Optional Output Control QueryStrings
+     *         @param 	Sample	        Type	    Description
+     *  @param id 	    true	        boolean	    Set as true to show mongo document id's
+     *  @param limit 	3	            integer	    Limit results returned, defaults to all documents returned
+     *  @param offset 	3	            integer	    Offset or skip results from the beginning of the query
+     *  @param sort 	flight_number	string	    Change result sorting by setting value to any parameter in this list
+     *  @param order 	desc	        string	    Change result ordering by setting values of asc or desc
+     */
     @WorkerThread
     @GET("launches/past")
     suspend fun getPastLaunches(
-        @Query("limit") limit: Int,
-        @Query("offset") offset: Int,
+        @Query("id") id: Boolean = false,
+        @Query("limit") limit: Int = 15,
+        @Query("offset") offset: Int = 0,
+        @Query("sort") sort: String? = null,
+        @Query("order") order: String = "desc",
     ): List<Launch>
 
 }
