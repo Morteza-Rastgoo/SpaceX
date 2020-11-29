78a07d4d9d3ae31f0929372ee3003a71b32c85b3
Morteza <mmortezarastgoo@gmail.com>
2020-11-29 13:45:44 +0330

    Add SuspendUseCase.kt to be used as base class for use cases

---
+++ app/src/main/java/com/dynamo/spacex/data/usecase/base/SuspendUseCase.kt
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2019 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.dynamo.spacex.data.usecase.base
+
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.withContext
+/**
+ * Executes business logic synchronously or asynchronously using Coroutines.
+ *
+ * The [execute] method of [SuspendUseCase] is a suspend function as opposed to the
+ * [UseCase.execute] method of [UseCase].
+ */
+abstract class SuspendUseCase<in P, R>(private val coroutineDispatcher: CoroutineDispatcher) {
+
+    /** Executes the use case asynchronously and returns a [Result].
+     *
+     * @return a [Result].
+     *
+     * @param parameters the input parameters to run the use case with
+     */
+    suspend operator fun invoke(parameters: P): R {
+        return withContext(coroutineDispatcher) {
+            execute(parameters)
+        }
+    }
+
+    /**
+     * Override this to set the code to be executed.
+     */
+    @Throws(RuntimeException::class)
+    protected abstract suspend fun execute(parameters: P): R
+}
