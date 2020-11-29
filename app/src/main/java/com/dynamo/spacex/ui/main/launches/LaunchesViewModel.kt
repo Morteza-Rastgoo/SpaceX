d170be7b09d7ff8fce647a1b1fc258fb9e69a58c
Morteza <mmortezarastgoo@gmail.com>
2020-11-29 23:10:04 +0330

    Move Current page variable to liveData

--- app/src/main/java/com/dynamo/spacex/ui/main/launches/LaunchesViewModel.kt
+++ app/src/main/java/com/dynamo/spacex/ui/main/launches/LaunchesViewModel.kt
@@ -31,29 +31,29 @@ class LaunchesViewModel @Inject constructor(private val getPastLaunchesUseCase: 
     /**
      * Current page of pagination
      */
-    private var _currentPage = 0
-    val currentPage: Int
+    private var _currentPage = MutableLiveData(0)
+    val currentPage: LiveData<Int>
         get() = _currentPage
 
     /**
      * Resets the pagination when needed
      */
-    fun resetPagination(): Unit {
-        _currentPage = 0
+    fun resetPagination() {
+        _currentPage.value = 0
     }
 
     /**
      * Loads past launches. This will also handle the pagination.
      */
     fun getPastLaunches() {
-        val offset = currentPage * dataLimit
-        launchDataLoad(if (currentPage == 0) ViewState.LOADING else ViewState.LOAD_MORE) {
+        val offset = currentPage.value!! * dataLimit
+        launchDataLoad(if (currentPage.value == 0) ViewState.LOADING else ViewState.LOAD_MORE) {
             getPastLaunchesUseCase.invoke(offset).doOnSuccess {
                 _pastLaunches.apply {
                     value?.addAll(it)
                     notifyObservers()
                 }
-                _currentPage++
+                _currentPage.value = currentPage.value!! + 1
             }
         }
     }
